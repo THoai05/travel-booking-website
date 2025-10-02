5b6d4ce4622361b59aee970b64dcab205be4ec9f
tuanbmt753 <xuantuanbmt753@gmail.com>
2025-10-02 21:10:59 +0700

    Tao API dang nhap thanh cong

--- BookingHotel-be/src/app.module.ts
+++ BookingHotel-be/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';  // <-- import UsersModule
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('DB_HOST') ?? 'localhost',
+        port: parseInt(config.get<string>('DB_PORT') ?? '3306', 10),
+        username: config.get<string>('DB_USER') ?? 'root',
+        password: config.get<string>('DB_PASS') ?? '',
+        database: config.get<string>('DB_NAME') ?? 'booking_hotel',
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+	UsersModule,  // <-- thêm vào đây
+	
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
