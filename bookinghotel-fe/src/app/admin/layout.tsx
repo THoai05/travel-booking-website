9d67fc93e97864a5146bc47bfa7fa7f037032970
THoai05 <vothanhhoai2k5@gmail.com>
2025-10-07 19:37:02 +0700

    cau truc source fe

--- bookinghotel-fe/src/app/admin/layout.tsx
+++ bookinghotel-fe/src/app/admin/layout.tsx
@@ -4,19 +4,7 @@ import Link from 'next/link';
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex min-h-screen">
-            <aside className="w-64 bg-gray-800 text-white p-4 flex flex-col justify-between">
-                <div>
-                    <h2 className="text-xl mb-6">Admin Dashboard</h2>
-                    <nav>
-                        <ul className="space-y-2">
-                            <li><Link href="/admin/accounts">Quản lý Acc</Link></li>
-                            <li><Link href="/admin/games">Quản lý Game</Link></li>
-                            <li><Link href="/admin/orders">Quản lý Đơn hàng</Link></li>
-                            <li><Link href="/admin/users">Quản lý User</Link></li>
-                        </ul>
-                    </nav>
-                </div>
-            </aside>
+            <Sidebar></Sidebar>
             <main className="flex-grow p-6 bg-gray-100">{children}</main>
         </div>
     );
