bad97b3cd3dab95dd59a604308bcc8635feeb572
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-10-08 17:03:22 +0700

    add custom breakpoint for responsive in global css file

--- bookinghotel-fe/tailwind.config.mjs
+++ bookinghotel-fe/tailwind.config.mjs
@@ -6,21 +6,30 @@ import animatePlugin from "tailwindcss-animatecss";
 module.exports = {
   content: ["./app/**/*.{ts,tsx,js,jsx}", "./components/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["var(--font-inter)", "ui-sans-serif", "system-ui", "-apple-system", "Segoe UI", "Roboto", "Helvetica Neue", "Arial"],
-        heading: ["var(--font-poppins)", "var(--font-inter)", "ui-sans-serif"]
-      }
-    },
     screens: {
       sm: "480px",
       md: "768px",
       lg: "976px",
       xl: "1440px",
-      "xl-max": { raw: "(max-width: 1440px)" },
-      "lg-max": { raw: "(max-width: 1024px)" },
-      "md-max": { raw: "(max-width: 768px)" },
-      "sm-max": { raw: "(max-width: 576px)" },
+      "xl-max": { max: "1440px" },
+      "lg-max": { max: "1024px" },
+      "md-max": { max: "768px" },
+      "sm-max": { max: "576px" },
+    },
+    extend: {
+      fontFamily: {
+        sans: [
+          "var(--font-inter)",
+          "ui-sans-serif",
+          "system-ui",
+          "-apple-system",
+          "Segoe UI",
+          "Roboto",
+          "Helvetica Neue",
+          "Arial",
+        ],
+        heading: ["var(--font-poppins)", "var(--font-inter)", "ui-sans-serif"],
+      },
     },
   },
   plugins: [
