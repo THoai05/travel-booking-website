73993596eb110493b26af6486f637cdf689827dc
THoai05 <tamvo10031962@gmail.com>
2025-10-10 23:05:32 +0700

    Update index.ts

--- bookinghotel-fe/src/constants/index.ts
+++ bookinghotel-fe/src/constants/index.ts
@@ -1,5 +1,5 @@
 export const NAV_LINKS = [
-  { href: '/', key: 'home', label: 'Trang chủ' },
+  { href: '/client', key: 'home', label: 'Trang chủ' },
   { href: '/client/hotels', key: 'hotels', label: 'Khách Sạn' },
   { href: '/rooms', key: 'rooms', label: 'Phòng' },
   { href: '/about', key: 'about', label: 'Giới thiệu' },
