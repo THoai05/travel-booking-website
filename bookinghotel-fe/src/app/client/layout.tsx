107dbcf10cbeb049e4db75f32612c5031be7c725
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-10-12 19:55:47 +0700

    add ScrollToTop button

--- bookinghotel-fe/src/app/client/layout.tsx
+++ bookinghotel-fe/src/app/client/layout.tsx
@@ -1,11 +1,14 @@
 import Header from "../client/components/layout/Header";
 import Footer from "../client/components/layout/Footer";
+import ScrollToTopButton from "../client/components/common/ScrollToTopButton";
+
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Header />
       <main>{children}</main>
       <Footer />
+      <ScrollToTopButton />
     </>
   );
 }
