f6c2375b5c6386ee1b44b738edebe749458455bd
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-10-15 14:58:12 +0700

    feat: add description section in blog detail

--- bookinghotel-fe/src/app/client/blog/[slug]/page.tsx
+++ bookinghotel-fe/src/app/client/blog/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import DetailDescription from "./DetailDescription";
 import Gallery from "./Gallery";
 
 export default function DetailBlog({ params }: { params: { slug: string } }) {
@@ -5,6 +6,7 @@ export default function DetailBlog({ params }: { params: { slug: string } }) {
     <section className="max-w-[1200px] mx-auto py-16 px-4">
       <h1 className="text-3xl font-bold mb-6 capitalize">{params.slug}</h1>
       <Gallery />
+      <DetailDescription />
     </section>
   );
 }
