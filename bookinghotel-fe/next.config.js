b2dec45e3a327ace906e315961a92033d4e6ec18
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-10-19 20:14:31 +0700

    temp commit

--- bookinghotel-fe/next.config.js
+++ bookinghotel-fe/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
+    domains: ['images.unsplash.com', 'picsum.photos'],
   },
   async rewrites() {
     return [
@@ -12,4 +12,5 @@ const nextConfig = {
     ]
   },
 };
+
 module.exports = nextConfig;
