499e8b9cdc21ffbcff8221b357b995cde9d4baf4
quandang08 <22211TT1369@mail.tdc.edu.vn>
2025-10-28 17:00:22 +0700

    feat(reviews) : create review api

--- BookingHotel-be/src/managements/reviews/controllers/reviews.controller.ts
+++ BookingHotel-be/src/managements/reviews/controllers/reviews.controller.ts
@@ -1,13 +1,31 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { ReviewsService } from '../services/reviews.service';
+import { CreateReviewDto } from '../dtos/create-review.dto';
+import { Request } from 'express';
+import { UsersService } from 'src/managements/users/services/users.service';
+import { JwtAuthGuard } from 'src/managements/auth/guards/jwt-auth.guard';
 
 @Controller('reviews')
 export class ReviewsController {
-  constructor(private readonly reviewsService: ReviewsService) { }
-  
+  constructor(
+    private readonly reviewsService: ReviewsService,
+    private readonly userService: UsersService
+  ) { }
+
+
   @Get('hotel/:id')
-  async handleGetReviewsByHotelId(@Param('id',ParseIntPipe) id:number) {
-    return await this.reviewsService.getReviewsByHotelId(id)
+  async handleGetReviewsByHotelId(@Param('id', ParseIntPipe) id: number) {
+    return await this.reviewsService.getReviewsByHotelId(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async createReview(@Body() dto: CreateReviewDto, @Req() req: Request) {
+    const userId = req.user?.userId;
+    if (!userId) {
+      throw new UnauthorizedException('User not found in request');
+    }
+    console.log('Creating review for userId:', userId);
+    return await this.reviewsService.createReview(dto, userId);
   }
 }
-  
--- BookingHotel-be/src/managements/reviews/services/reviews.service.ts
+++ BookingHotel-be/src/managements/reviews/services/reviews.service.ts
@@ -1,27 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Review } from '../entities/review.entity';
 import { Repository } from 'typeorm';
+import { CreateReviewDto } from '../dtos/create-review.dto';
+import { User } from 'src/managements/users/entities/users.entity';
+import { Hotel } from 'src/managements/hotels/entities/hotel.entity';
 
 @Injectable()
 export class ReviewsService {
     constructor(
         @InjectRepository(Review)
-        private readonly reviewRepo:Repository<Review>
+        private readonly reviewRepo: Repository<Review>,
+
+        @InjectRepository(User)
+        private readonly userRepo: Repository<User>,
+
+        @InjectRepository(Hotel)
+        private readonly hotelRepo: Repository<Hotel>
     ) { }
-    
-    async getSummaryReviewByHotelId(hotelId: number):Promise<{avgRating:number,reviewCount:number}> {
+
+    async getSummaryReviewByHotelId(hotelId: number): Promise<{ avgRating: number, reviewCount: number }> {
         const summary = await this.reviewRepo
             .createQueryBuilder('reviews')
             .select([
                 'AVG(reviews.rating) AS avgRating',
                 'COUNT(reviews.id) AS reviewCount'
             ])
-            .where('reviews.hotelId = :hotelId',{hotelId})
+            .where('reviews.hotelId = :hotelId', { hotelId })
             .getRawOne()
         return {
             avgRating: Number(Number(summary?.avgRating || 0).toFixed(2)),
-            reviewCount:Number(summary?.reviewCount||0)
+            reviewCount: Number(summary?.reviewCount || 0)
         }
     }
 
@@ -30,12 +39,12 @@ export class ReviewsService {
             .createQueryBuilder('review')
             .leftJoinAndSelect('review.user', 'user')
             .select([
-            'review.id',
-            'review.rating',
-            'review.comment',
-            'review.createdAt',
-            'user.id',
-            'user.username',
+                'review.id',
+                'review.rating',
+                'review.comment',
+                'review.createdAt',
+                'user.id',
+                'user.username',
             ])
             .where('review.reviewType = :type', { type: 'hotel' })
             .andWhere('review.hotelId = :hotelId', { hotelId })
@@ -44,6 +53,26 @@ export class ReviewsService {
         const [data, total] = await query.getManyAndCount();
 
         return { data, total };
+    }
+
+    async createReview(dto: CreateReviewDto, userId: number) {
+        const user = await this.userRepo.findOne({ where: { id: userId } });
+        const hotel = await this.hotelRepo.findOne({ where: { id: dto.hotelId } });
+
+        if (!user || !hotel) {
+            throw new NotFoundException('User or Hotel not found');
+        }
+
+        const review = this.reviewRepo.create({
+            user,
+            hotel,
+            rating: dto.rating,
+            comment: dto.comment,
+            reviewType: dto.reviewType,
+            images: dto.images ? JSON.stringify(dto.images) : null,
+        });
+
+        return await this.reviewRepo.save(review);
     }
 
 }
