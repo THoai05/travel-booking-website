79287cfc157bd5f7b55302dd75d29096c881b862
haus0409 <haconghau07@gmail.com>
2025-11-02 18:11:08 +0700

    fix package

--- BookingHotel-be/src/managements/auth/guards/jwt-auth.guard.ts
+++ BookingHotel-be/src/managements/auth/guards/jwt-auth.guard.ts
@@ -1,10 +1,21 @@
-import { ExecutionContext, Injectable } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-import { Observable } from "rxjs";
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        return super.canActivate(context)
-    }
+export class JwtAuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+ async canActivate(context: ExecutionContext): Promise<boolean> {
+  const request = context.switchToHttp().getRequest();
+  const token = request.cookies?.access_Token;
+  if (!token) throw new UnauthorizedException('Missing token');
+
+  try {
+    const payload = await this.jwtService.verifyAsync(token);
+    request.user = payload;
+    return true;
+  } catch {
+    throw new UnauthorizedException('Invalid or expired token');
+  }
+}
 }
